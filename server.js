3257a5958ccc6b697f216a82fc5980618ec8f724
Serhii <mel.gipppson@gmail.com>
2022-12-12 20:47:39 +0200

    created not-found and error-handler middlewares

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express'
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
 app.get('/', (req, res) => {
+    // throw new Error('some error')
     res.send('Welcome!')
 })
+
+app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware)
 
 app.listen(PORT, () => {
     console.log(`Server worked on PORT: ${PORT}`);
