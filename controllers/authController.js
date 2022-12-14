708b4e5889e45aba9b9669011aa67210e388f00b
Serhii <mel.gipppson@gmail.com>
2022-12-14 09:11:01 +0200

    Completed error handler logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,33 @@
+import { StatusCodes } from 'http-status-codes';
 import User from "../model/User.js"
 
+class CustomAPIError extends Error {
+    constructor(message) {
+        super(message)
+    }
+}
+
+class BadRequest extends CustomAPIError {
+    constructor(message) {
+        super(message)
+        this.statusCode = StatusCodes.BAD_REQUEST
+    }
+}
+
+class NotFoundError extends CustomAPIError {
+    constructor(message) {
+        super(message)
+        this.statusCode = StatusCodes.NOT_FOUND
+    }
+}
+
 const register = async (req, res) => {
-    const user = await User.create(req.body)
-    res.status(201).json({user})
+    const {name, password, email} = req.body
+    if(!name || !email || !password){
+        throw new BadRequest('Please provide all values')
+    }
+    const user = await User.create({name, password, email})
+    res.status(StatusCodes.CREATED).json({user})
 }
 
 const login = async (req, res) => {
