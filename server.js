9b25079afd57d692460ccfc5650fe857d8f43569
Serhii <mel.gipppson@gmail.com>
2022-12-14 15:46:43 +0200

    instead of cors used approach with proxy(only if i want to host my app on one place both of clien and server part )

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import 'express-async-errors'
 import express from 'express'
-import cors from 'cors'
 import dotenv from 'dotenv'
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
@@ -12,12 +11,15 @@ dotenv.config()
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json({msg: 'Welcome!'})
 })
+app.get('/api/v1', (req, res) => {
+    res.json({msg: 'API!'})
+})
+
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', jobsRouter)
 app.use(errorHandlerMiddleware)
