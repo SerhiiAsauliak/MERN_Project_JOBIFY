872238746e771edda0983f92d6051c13fd7a815b
Serhii <mel.gipppson@gmail.com>
2022-12-14 12:51:43 +0200

    installed bcript, and added hashing logic into User model(by pre middleware)

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,13 +4,17 @@ import BadRequestError from './../errors/bad_request.js';
 
 const register = async (req, res) => {
     const {name, password, email} = req.body
+
     if(!name || !email || !password){
         throw new BadRequestError('Please provide all values')
     }
-    const userAlreadyExist = User.findOne(email)
+
+    const userAlreadyExist = await User.findOne({email})
+
     if(userAlreadyExist){
-        throw new BadRequestError('this email already use')
+        throw new BadRequestError('this email already in use')
     }
+
     const user = await User.create({name, password, email})
     res.status(StatusCodes.CREATED).json({user})
 }
@@ -18,6 +22,7 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     res.send('login')
 }
+
 const updateUser = async (req, res) => {
     res.send('updateUser')
 }
