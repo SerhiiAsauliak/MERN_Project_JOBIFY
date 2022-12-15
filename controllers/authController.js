45eee24cf257f7fec9141415238c76b6f025fc69
Serhii <mel.gipppson@gmail.com>
2022-12-15 17:44:25 +0200

    deleted git from client folder

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,12 +18,15 @@ const register = async (req, res) => {
     const user = await User.create({name, password, email})
     const token = user.createJWT()
     res.status(StatusCodes.CREATED)
-        .json({user:{
-            email:user.email,
-            lastName:user.lastName,
-            location:user.location,
-            name:user.name,
-        }, token})
+        .json({
+            user:{
+                email:user.email,
+                lastName:user.lastName,
+                location:user.location,
+                name:user.name,
+            }, 
+            token,
+            location: user.location})
 }
 
 const login = async (req, res) => {
