e5d6fb8978595c10b7add60e98767702322f00fb
Serhii <mel.gipppson@gmail.com>
2023-01-02 10:53:01 +0200

    alternative search approach (own debounce logic)

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,8 +1,11 @@
 import { FormRow, FormRowSelect } from '.'
 import { useAppContext } from '../context/appContext'
 import Wrapper from '../assets/wrappers/SearchContainer'
+import { useState, useMemo } from 'react';
 
 const SearchContainer = () => {
+  const [localSearch, setLocalSearch] = useState('')
+
   const {
     isLoading,
     search,
@@ -17,13 +20,26 @@ const SearchContainer = () => {
   } = useAppContext()
 
   const handleSearch = (e) => {
-    if (isLoading) return;
-    handleChange({ name: e.target.name, value: e.target.value });
+    handleChange({ name: e.target.name, value: e.target.value })
   }
   const handleSubmit =(e) => {
     e.preventDefault()
+    setLocalSearch('')
     clearFilters()
   }
+
+  const debounce = () => {
+    let timeoutId
+    return (e) => {
+      setLocalSearch(e.target.value)
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(() => {
+        handleChange({ name: e.target.name, value: e.target.value })
+      }, 1000)
+    }
+  }
+
+  const optimizedDebounce = useMemo(() => debounce(), [])
 
   return (
     <Wrapper>
@@ -34,8 +50,8 @@ const SearchContainer = () => {
           <FormRow
             type='text'
             name='search'
-            value={search}
-            handleChange={handleSearch}
+            value={localSearch}
+            handleChange={optimizedDebounce}
           >
           </FormRow>
           {/* search by status */}
